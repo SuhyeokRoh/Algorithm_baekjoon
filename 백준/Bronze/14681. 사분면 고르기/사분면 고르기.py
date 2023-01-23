e43b0772010a2a372a61e28f5d39108d44d63826
SuhyeokRoh <122436950+SuhyeokRoh@users.noreply.github.com>
2023-01-23 23:38:36 +0900

    [Bronze V] Title: 사분면 고르기, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.py
@@ -0,0 +1,11 @@
+x = int(input())
+y = int(input())
+match (x>0, y>0):
+    case (True, True):
+        print("1")
+    case (True, False):
+        print("4")
+    case (False, True):
+        print("2")
+    case (False, False):
+        print("3")        
