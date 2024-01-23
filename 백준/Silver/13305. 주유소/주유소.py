8255b5eb5830b5aeafc474d9eb7bfe3f795f738e
Nosu <122436950+SuhyeokRoh@users.noreply.github.com>
2024-01-23 16:31:57 +0900

    [Silver III] Title: 주유소, Time: 160 ms, Memory: 46228 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -2,20 +2,18 @@ import sys
 
 n = int(sys.stdin.readline().rstrip())
 distance = list(map(int, sys.stdin.readline().split()))
-city = list(map(int, sys.stdin.readline().split()))
-minV, maxV = city.index(min(city[:-1])), city.index(max(city[:-1]))
-x, price = 0, 0
+price = list(map(int, sys.stdin.readline().split()))
+minimum = price.index(min(price[:-1]))
+rst, index = 0, 0
 
-while True:
-    if x == minV:
-        price += city[x] * sum(distance[x:])
+while index < n:
+    if index == minimum:
+        rst += sum(distance[index:n-1]) * price[index]
         break
-    elif x == maxV:
-        price += city[x] * distance[x]
-        x += 1
     else:
-        tmp = city.index(min(city[x+1:-1]))
-        price += city[x] * sum(distance[x:tmp])
-        x = tmp
-
-print(price)
+        i = 1
+        while price[index] < price[index+i] and index + i != minimum:
+            i += 1
+        rst += sum(distance[index:index+i]) * price[index]
+        index += i
+print(rst)
